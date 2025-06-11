3a873921998842e1b3a0c752766ad6b29f9f74b0
ast. <ast@over-sky.net>
2025-06-11 19:48:53 +0900

    :art: welcomeページを削除し、空のページを設置

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,5 +22,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Model::shouldBeStrict(! $this->app->isProduction());
+
+        // view()の対象にviews/pagesディレクトリを追加
+        View::getFinder()->prependLocation(resource_path('views/pages'));
     }
 }
